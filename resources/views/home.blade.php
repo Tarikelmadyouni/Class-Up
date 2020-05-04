634e0ee79e24f44208a56b87412bf7626a78fe59
jedafinal <jedafinal@hotmail.com>
2020-05-04 17:00:32 +0200

    tchat v1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -81,7 +81,7 @@
 
             </div>
         </div>
-
+  @can('manage-user')
       <script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer>
   new Crate({
     server: '706499274338336800',
@@ -89,6 +89,7 @@
     shard: 'https://e.widgetbot.io'
   })
 </script>
+ @endcan
     </div>
 </div>
 @endsection

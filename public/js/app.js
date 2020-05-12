f053c0dca6139041b16ec3ec6fc82e62fa416243
Your Name <you@example.com>
2020-05-12 10:03:05 +0200

    màj

--- public/js/app.js
+++ public/js/app.js
@@ -53774,8 +53774,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\jedaf\Desktop\class-up-stephane\Class-Up\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\jedaf\Desktop\class-up-stephane\Class-Up\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\class-up-fork\Class-Up\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\class-up-fork\Class-Up\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
